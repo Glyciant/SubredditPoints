ebd98bab756d6210b2b5ecb95a492c11f04fc232
Heep <contact.heep@gmail.com>
2017-05-27 20:32:55 +0100

    Fixed #4 Again

--- pub/js/transactions.js
+++ pub/js/transactions.js
@@ -20,7 +20,7 @@ $(document).delegate("#get-transactions", "click", function() {
             }
             data[0].id = data[0].timestamp;
             data[0].timestamp = new Date(data[0].timestamp);
-            $("#transactions-table table tbody").append("<tr><td>" + data[0].id + "</td><td>" + data[0].timestamp.getDate() + "/" + (data[0].timestamp.getMonth() + 1) + "/" + data[0].timestamp.getFullYear() + "</td><td>" + data[0].from + "</td><td>" + data[0].title + "</td><td>" + data[0].description + "</td><td>" + data[0].difference + "</td><td>" + data[0].mod_note + "</td></tr>");
+            $("#transactions-table table tbody").append("<tr class='row' style='margin: 10px 0;' style='margin: 10px 0;'><td class='col s2' style='margin: 10px 0;'>" + data[0].id + "</td><td class='col s1' style='margin: 10px 0;'>" + data[0].timestamp.getDate() + "/" + (data[0].timestamp.getMonth() + 1) + "/" + data[0].timestamp.getFullYear() + "</td><td class='col s2' style='margin: 10px 0;'>" + data[0].from + "</td><td class='col s2' style='margin: 10px 0;'>" + data[0].title + "</td><td class='col s1' style='margin: 10px 0;'>" + data[0].description + "</td><td class='col s1' style='margin: 10px 0;'>" + data[0].difference + "</td><td class='col s3' style='margin: 10px 0;'>" + data[0].mod_note + "</td></tr>");
           }
           else {
             for (var i in data) {
@@ -33,7 +33,7 @@ $(document).delegate("#get-transactions", "click", function() {
                 }
                 data[i][0].id = data[i][0].timestamp;
                 data[i][0].timestamp = new Date(data[i][0].timestamp);
-                $("#transactions-table table tbody").append("<tr><td>" + data[i][0].id + "</td><td>" + data[i][0].timestamp.getDate() + "/" + (data[i][0].timestamp.getMonth() + 1) + "/" + data[i][0].timestamp.getFullYear() + "</td><td>" + data[i][0].from + "</td><td>" + data[i][0].title + "</td><td>" + data[i][0].description + "</td><td>" + data[i][0].difference + "</td><td>" + data[i][0].mod_note + "</td></tr>");
+                $("#transactions-table table tbody").append("<tr class='row' style='margin: 10px 0;'><td class='col s2' style='margin: 10px 0;'>" + data[i][0].id + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][0].timestamp.getDate() + "/" + (data[i][0].timestamp.getMonth() + 1) + "/" + data[i][0].timestamp.getFullYear() + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][0].from + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][0].title + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][0].description + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][0].difference + "</td><td class='col s3' style='margin: 10px 0;'>" + data[i][0].mod_note + "</td></tr>");
               }
               else {
                 for (var j in data[i]) {
@@ -50,9 +50,9 @@ $(document).delegate("#get-transactions", "click", function() {
                     for (var k in data[i]) {
                       difference += data[i][k].difference;
                     }
-                    $("#transactions-table table tbody").append("<tr id='group-header' data-id='" + data[i][j].id + "'><td id='id-field'><i class='fa fa-chevron-right' aria-hidden='true'></td><td>" + data[i][j].timestamp.getDate() + "/" + (data[i][j].timestamp.getMonth() + 1) + "/" + data[i][j].timestamp.getFullYear() + "</td><td>" + data[i][j].from + "</td><td>" + data[i][j].title + "</td><td>List of Transactions</td><td>" + difference + "</td><td>-</td></tr>");
+                    $("#transactions-table table tbody").append("<tr id='group-header' data-id='" + data[i][j].id + "' class='row' style='margin: 10px 0;'><td id='id-field' class='col s2' style='margin: 10px 0;'><i class='fa fa-chevron-right' aria-hidden='true'></td><td class='col s1' style='margin: 10px 0;'>" + data[i][j].timestamp.getDate() + "/" + (data[i][j].timestamp.getMonth() + 1) + "/" + data[i][j].timestamp.getFullYear() + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][j].from + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][j].title + "</td><td class='col s1' style='margin: 10px 0;'>List of Transactions</td><td class='col s1' style='margin: 10px 0;'>" + difference + "</td><td class='col s3' style='margin: 10px 0;'>-</td></tr>");
                   }
-                  $("#transactions-table table tbody").append("<tr class='group-data-" +  data[i][0].id + "' style='display: none;'><td>" + data[i][j].id + "</td><td>" + data[i][j].timestamp.getDate() + "/" + (data[i][j].timestamp.getMonth() + 1) + "/" + data[i][j].timestamp.getFullYear() + "</td><td>" + data[i][j].from + "</td><td>" + data[i][j].title + "</td><td>" + data[i][j].description + "</td><td>" + data[i][j].difference + "</td><td>" + data[i][j].mod_note + "</td></tr>");
+                  $("#transactions-table table tbody").append("<tr class='group-data-" +  data[i][0].id + "' style='display: none;' class='row' style='margin: 10px 0;'><td class='col s2' style='margin: 10px 0;'>" + data[i][j].id + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][j].timestamp.getDate() + "/" + (data[i][j].timestamp.getMonth() + 1) + "/" + data[i][j].timestamp.getFullYear() + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][j].from + "</td><td class='col s2' style='margin: 10px 0;'>" + data[i][j].title + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][j].description + "</td><td class='col s1' style='margin: 10px 0;'>" + data[i][j].difference + "</td><td class='col s3' style='margin: 10px 0;'>" + data[i][j].mod_note + "</td></tr>");
                 }
               }
             }
